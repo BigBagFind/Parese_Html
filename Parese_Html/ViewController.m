e004998bc9b1e480f4824e57e54aa4f08cbb43e4
BigBagFind <594228924@qq.com>
2015-11-10 18:18:11 +0800

    git commit -"html简单解析"

--- Parese_Html/ViewController.m
+++ Parese_Html/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "TFHpple.h"
+#import "XPathQuery.h"
 
 @interface ViewController ()
 
@@ -16,7 +18,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //创建二进制字符串
+//    NSString *dataStr = [NSString stringWithContentsOfURL:[NSURL URLWithString:str] encoding:NSUTF8StringEncoding error:NULL];
+    //NSString *filePath = [[NSBundle mainBundle]pathForResource:@"File" ofType:@"html"];
+   // NSString *dataStr = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    NSString *str = @"\n<p class=\"xqsxbox1\">\n<img src=\"resources/images/xqy_tu1.gif\" height=\"299\" width=\"788\">\n</p>\n<p class=\"xqsxbox1\">\n<img src=\"resources/images/xqy_tu2.gif\">\n</p>\n<p class=\"xqsxbox1\">\n<img src=\"resources/images/xqy_tu3.gif\">\n</p>\n\n";
+    //转换二进制数据
+    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+//      NSData *data = [[NSData alloc] initWithContentsOfFile:@"example.html"];
+    // Create parser
+    TFHpple *xpathParser = [[TFHpple alloc] initWithHTMLData:data];
+    //Get all the cells of the 2nd row of the 3rd table
+//    NSArray *elements  = [xpathParser search:@"//table[3]/tr[2]/td"];
+//    NSArray *elements  = [xpathParser searchWithXPathQuery:nodeString];
+   // cd[@country]
+    NSArray *elements = [xpathParser searchWithXPathQuery:@"//img[@src]"];
+    // Access the first cell
+    TFHppleElement *element = [elements objectAtIndex:0];
+    // Get the text within the cell tag
+    //NSString *content = [element content];
+    NSLog(@"%@",element);
+    NSLog(@"%@",[elements objectAtIndex:1]);
+   // [xpathParser release];
+    //[data release];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
